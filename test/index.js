0043f24ba69c34ca85e288c2c6704672a201f500
Dmitry <df.creative@gmail.com>
2018-08-27 20:44:37 +0300

    Add uint data type

--- test/index.js
+++ test/index.js
@@ -48,13 +48,13 @@ test('version 300es', function(t) {
   }).map(function (t) {
     return t.data
   }), [ 'out', 'vec4', 'in', 'vec2', 'uniform', 'usampler2DArray', 'void', 'vec4', 'vec4' ], 'matches 300es tokens')
-  
+
   t.deepEqual(tokens.filter(function (t) {
     return t.type === 'builtin'
   }).map(function (t) {
     return t.data
   }), [ 'textureLod' ], 'matches 300es builtins')
-  
+
   t.end()
 })
 
@@ -108,6 +108,26 @@ test('floats: should recognize negative exp', function (t) {
     { type: 'operator', data: '-', position: 3, line: 1, column: 4 },
     { type: 'float', data: '2.0', position: 4, line: 1, column: 7 },
     { type: 'eof', data: '(eof)', position: 4, line: 1, column: 7 }
+  ]);
+
+  t.end();
+});
+
+test('uint / int data types', function (t) {
+  t.deepEqual(tokenizeString('uint x;'), [
+    {type: "keyword", data: "uint", position: 0, line: 1, column: 4},
+    {type: "whitespace", data: " ", position: 4, line: 1, column: 5},
+    {type: "ident", data: "x", position: 5, line: 1, column: 6},
+    {type: "operator", data: ";", position: 6, line: 1, column: 6},
+    {type: "eof", data: "(eof)", position: 6, line: 1, column: 7}
+  ]);
+
+  t.deepEqual(tokenizeString('int x;'), [
+    {type: "keyword", data: "int", position: 0, line: 1, column: 3},
+    {type: "whitespace", data: " ", position: 3, line: 1, column: 4},
+    {type: "ident", data: "x", position: 4, line: 1, column: 5},
+    {type: "operator", data: ";", position: 5, line: 1, column: 5},
+    {type: "eof", data: "(eof)", position: 5, line: 1, column: 6}
   ]);
 
   t.end();
